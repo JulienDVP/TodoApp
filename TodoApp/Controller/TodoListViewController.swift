001b9541614cdcb27b1435378eed251198f02d64
Julien <julien.r.dvp@gmail.com>
2019-10-18 16:26:47 +0200

    Persist Data Encoder

--- TodoApp/Controller/TodoListViewController.swift
+++ TodoApp/Controller/TodoListViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let defaults = UserDefaults.standard
-    var itemArray = ["Learn Swift", "T-Nice", "Voiture", "Infirmier"]
-
+    
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        loadItems()
+        
+
     }
 //MARK: - TableView Datasources Methods
     
@@ -30,7 +30,20 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        // TERNARY OPERATOR =
+        // value = condition ? ValueIfTrue ?? ValueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+//        PAREIL QUEE 
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         
         return cell
     }
@@ -41,12 +54,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType  = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType  = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -59,9 +69,13 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new todo Button", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // Ce qui va se passer quand il cliquera sur ce bouton
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
@@ -70,6 +84,34 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Save Data Methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do  {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            print("Erreur pendant l'encoding de l'array \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    //MARK: - loadItems
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("erreur de decodage \(error)")
+            }
+        }
     }
     
 }
---
+++ TodoApp/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  TodoApp
+//
+//  Created by Julien on 18/10/2019.
+//  Copyright © 2019 Julien. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
